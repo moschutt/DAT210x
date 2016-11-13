99152b7f7a24c24138102a6fe44a1dffe92be2ef
Mark Schutt <moschutt@gmail.com>
2016-11-13 05:01:03 +0000

    Sleepy Time

--- Module2/assignment3.py
+++ Module2/assignment3.py
@@ -1,39 +1,42 @@
+"""
+assignment3.py
+"""
 import pandas as pd
 
-# TODO: Load up the dataset
 # Ensuring you set the appropriate header column names
 #
 # .. your code here ..
+DATA = pd.read_csv('Datasets/servo.data',
+                   header=None,
+                   names=['motor', 'screw', 'pgain', 'vgain', 'class'])
 
 
-# TODO: Create a slice that contains all entries
-# having a vgain equal to 5. Then print the 
+# having a vgain equal to 5. Then print the
 # length of (# of samples in) that slice:
 #
 # .. your code here ..
+DATA_VGAIN = DATA[DATA.vgain == 5]
+print(len(DATA_VGAIN))
 
 
-# TODO: Create a slice that contains all entries
 # having a motor equal to E and screw equal
 # to E. Then print the length of (# of
 # samples in) that slice:
 #
 # .. your code here ..
+DATA_E = DATA[(DATA.motor == 'E') & (DATA.screw == 'E')]
+print(len(DATA_E))
 
 
-
-# TODO: Create a slice that contains all entries
 # having a pgain equal to 4. Use one of the
 # various methods of finding the mean vgain
 # value for the samples in that slice. Once
 # you've found it, print it:
 #
 # .. your code here ..
+DATA_V = DATA[DATA.pgain == 4]
+print(DATA_V.vgain.mean())
 
 
-
-# TODO: (Bonus) See what happens when you run
 # the .dtypes method on your dataframe!
-
-
-
+print(DATA.dtypes)
--- Module2/assignment4.py
+++ Module2/assignment4.py
@@ -6,17 +6,21 @@ import pandas as pd
 # carefully at the sample code provided in the reading
 #
 # .. your code here ..
+DATA = pd.read_html('http://www.espn.com/nhl/statistics/player/_/stat/points/sort/points/year/2015/seasontype/2')
+DATA = DATA[0]
+DATA = DATA.loc[2:, :]
 
 
-# TODO: Rename the columns so that they match the
 # column definitions provided to you on the website
 #
 # .. your code here ..
-
+DATA.columns = ['RK', 'PLAYER', 'TEAM', 'GP', 'G', 'A', 'PTS', '+/-', 'PIM',
+                'PTS/G', 'SOG', 'PCT', 'GWG', 'PP-G', 'PP-A', 'SH-G', 'SH-A']
 
 # TODO: Get rid of any row that has at least 4 NANs in it
 #
 # .. your code here ..
+DATA = DATA.dropna(axis=0, thresh=4)
 
 
 # TODO: At this point, look through your dataset by printing
@@ -25,23 +29,38 @@ import pandas as pd
 # EXCEPT those rows?
 #
 # .. your code here ..
+DATA = DATA[DATA.RK != 'RK']
 
 
 # TODO: Get rid of the 'RK' column
 #
 # .. your code here ..
+DATA = DATA.iloc[:, 1:]
 
 
 # TODO: Ensure there are no holes in your index by resetting
 # it. By the way, don't store the original index
 #
 # .. your code here ..
-
+DATA.reset_index(inplace=True)
 
 
 # TODO: Check the data type of all columns, and ensure those
 # that should be numeric are numeric
-
+DATA.GP = pd.to_numeric(DATA.GP)
+DATA.G = pd.to_numeric(DATA.G)
+DATA.A = pd.to_numeric(DATA.A)
+DATA.PTS = pd.to_numeric(DATA.PTS)
+DATA.loc[:, '+/-'] = pd.to_numeric(DATA.loc[:, '+/-'])
+DATA.PIM = pd.to_numeric(DATA.PIM)
+DATA.loc[:, 'PTS/G'] = pd.to_numeric(DATA.loc[:, 'PTS/G'])
+DATA.SOG = pd.to_numeric(DATA.SOG)
+DATA.GWG = pd.to_numeric(DATA.GWG)
+DATA.PCT = pd.to_numeric(DATA.PCT)
+DATA.loc[:, 'PP-G'] = pd.to_numeric(DATA.loc[:, 'PP-G'])
+DATA.loc[:, 'PP-A'] = pd.to_numeric(DATA.loc[:, 'PP-A'])
+DATA.loc[:, 'SH-G'] = pd.to_numeric(DATA.loc[:, 'SH-G'])
+DATA.loc[:, 'SH-A'] = pd.to_numeric(DATA.loc[:, 'SH-A'])
 
 
 # TODO: Your dataframe is now ready! Use the appropriate 
